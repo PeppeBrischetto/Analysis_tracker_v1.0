7194331f810fea41face5917dd0b67079f1e9b0e
numenEG <uni362183@studium.unict.it>
2025-09-11 09:43:23 +0200

    gain

--- Analysis_BR/gainSP.C
+++ Analysis_BR/gainSP.C
@@ -26,6 +26,21 @@ const Int_t NPads = 60;
 void gainSP(int run){
 
 //###########################################################################################################
+// Needed Variables
+   
+   TH1D *charge = new TH1D("charge","",1000,0,50000);
+   
+   Double_t mean_charge = 0.;
+   Double_t primaries = 0.;
+   Double_t w_gas = 0.;
+   Double_t solid_angle =0.;
+   Double_t rate = 0.;
+   Double_t t = 0.;
+   Double_t gain = 0.;
+   Double_t dE = 0.;
+   
+     
+//###########################################################################################################
 // OpenFiles
    openTrackFile(run);
    tree->Print();
@@ -58,6 +73,17 @@ void gainSP(int run){
 //###########################################################################################################
 // Data loop
    
+   for(Int_t i=0; i<entries; i++){
+      Double_t tot_charge = 0.;
+      //if(cutGa->IsInside(cl_x_mm[0], cl_x_mm[1])){
+      tot_charge = cl_charge[0]+cl_charge[1]+cl_charge[2]+cl_charge[3]+cl_charge[4]+cl_charge[5]+cl_charge[6]+cl_charge[7]+cl_charge[8]+cl_charge[9]+cl_charge[10];
+      charge->Fill(tot_charge)
+      //}
+   }
    
+   mean_charge = tot_charge->Fit("gaus","R+");
+   primaries = (dE/w_gas);
+   gain=tot_charge/primaries;
+   cout << "Gain: " << gain << endl;
 
 }
